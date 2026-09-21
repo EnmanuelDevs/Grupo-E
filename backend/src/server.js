b05ceeb41f02020cae135de89fbd8d909ae4d76a
Manuel Garcia De La Rosa <Manuel@MacBook-Air-3.local>
2026-09-21 18:23:32 -0400

    feat: conectar backend con MongoDB Atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+console.log(
+  "MONGO_URI cargada:",
+  process.env.MONGO_URI ? "Sí" : "No"
+);
+
 const PORT = process.env.PORT || 3000;
 
+connectDB().then(()=> {
 app.listen(PORT, () => {
     console.log(`Servior ejecutandose en el puerto: ${PORT}`);
-})
+});
+});
